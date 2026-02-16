520ac1eaf4aeb129525cef82ed726fc8c78da8df
Abdul Nafae Imran <nafaeimran123@gmail.com>
2026-02-16 23:17:36 +0500

    Added Class Square

---
+++ app/src/main/java/edu/lums/impact/Square.java
@@ -0,0 +1,17 @@
+package edu.lums.impact;
+
+public class Square extends Shape {
+    private int length;
+
+    public Square(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
